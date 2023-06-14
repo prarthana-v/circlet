b59cfedfd27705213d21db6be0f4651d60ca0581
prarthana-v <p.d.vaghani1729@gmail.com>
2023-06-14 07:23:55 +0530

    Pr-4

---
+++ CIRCLET5.C
@@ -0,0 +1,26 @@
+/*       5
+	454
+       34543
+      2345432
+     123454321
+*/
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+ int i,j,k,l;
+ clrscr();
+ for(i=5;i>=1;i--){
+   for(k=1;k<i;k++){
+    printf(" ");
+   }
+   for(j=i;j<=5;j++){
+   printf("%d",j);
+   }
+   for(l=4;l>=i;l--){
+   printf("%d",l);
+   }
+   printf("\n");
+ }
+getch();
+}
---
+++ Q.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+ int i;
+ clrscr();
+  for(i=1;i<=7;i++){
+   if(i==1) printf(" * * * *\n");
+   else if(i==2) printf("*       *\n");
+   else if(i==4) printf("*    *  * \n");
+   else if(i==5) printf("*     * * \n");
+   else if(i==6) printf(" * * * * \n");
+   else if(i==7) printf("        *\n");
+  }
+  getch();
+}
